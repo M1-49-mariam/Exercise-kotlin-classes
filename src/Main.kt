bb66dcbc13b9273c386b295dc5ed081c0e5da74d
M1-49-mariam <mariam.almutairi19@gmail.com>
2025-02-16 18:22:41 +0300

    done part 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,14 @@
 fun main() {
     val myBook = Book("Extremely Loud and Incredibly Close",
         "Jonathon", 2016)
+    myBook.getBookInfo()
 
 }
 
 class Book (  var title: String, var author: String, var yearPublished: Int ){
 
-
+    fun getBookInfo() {
+        println("$title by $author ($yearPublished)")
+    }
 
 }
