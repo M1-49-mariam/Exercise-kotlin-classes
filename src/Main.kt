29e48696a052758f9862368c35a5a56838071833
M1-49-mariam <mariam.almutairi19@gmail.com>
2025-02-16 17:25:44 +0300

    done part 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!")
+    val myBook = Book()
+
+}
+
+class Book {
+    var title: String = ""
+    var author: String = ""
+    var yearPublished: Int = 2000
 }
